edd778e1597ae53c50227b8727d9ed66292f6368
Makol Ariik <mawelbukringthii@gmail.com>
2022-09-22 00:35:31 +0300

    added int a = 34;               for(a = 24;a>4; a = a + 4)

--- src/Main.java
+++ src/Main.java
@@ -2,6 +2,8 @@ public class Main
 {
     public static void main(String[] args)
     {
-        System.out.println("Hello world!");
+        int a = 34;
+        for(a = 24;a>4; a = a + 4)
+        System.out.println(a);
     }
 }
